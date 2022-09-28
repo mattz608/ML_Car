566f7d59beeb5138169a98cd3c056c762531888e
Matt Zimmers <36614795+mattz608@users.noreply.github.com>
2022-09-27 19:04:50 -0500

    Added support for dummy cars. Added neural network. Added support for AI cars

--- main.js
+++ main.js
@@ -3,14 +3,19 @@ canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width/2, canvas.width*0.9)
-const car = new Car(road.getLaneCenter(1), 100, 30, 50);
-road.draw(ctx);
-car.draw(ctx);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI");
+const traffic = [
+    new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2)
+];
 
 animate();
 
 function animate() {
-    car.update(road.borders);
+    // Update objects
+    for (let i = 0; i < traffic.length; i++) {
+        traffic[i].update(road.borders,[]);
+    }
+    car.update(road.borders,traffic);
 
     canvas.height = window.innerHeight;
 
@@ -18,8 +23,12 @@ function animate() {
     ctx.save();
     ctx.translate(0, -car.y + canvas.height * 0.8);
 
+    // Draw objects
     road.draw(ctx);
-    car.draw(ctx);
+    for (let i = 0; i < traffic.length; i++) {
+        traffic[i].draw(ctx, "red");
+    }
+    car.draw(ctx, "blue");
 
     ctx.restore();
     requestAnimationFrame(animate);
